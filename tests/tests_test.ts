378afe4c7520ab8b67ab1e721eb7826dca53a793
DenchikKon <tweety121212@gmail.com>
2024-11-01 11:14:58 +0300

    Тесты по SmartDeal

--- tests/tests_test.ts
+++ tests/tests_test.ts
@@ -3,6 +3,7 @@ import { IAuthorizedUser, ICreateOrderService, IFlags, IOrderServiceModel } from
 import { get, request } from "http";
 import { url } from 'inspector';
 import { TIMEOUT } from 'dns';
+import { buffer } from 'stream/consumers';
 
 const config = require('../codecept.conf').config;
 const assert = require('assert');
@@ -614,14 +615,15 @@ Feature("Внесение документа паспорта в заявку Sm
 for (let i = 0; i < localSites.length; i++) {
     const site = localSites[i];
     Scenario(`SendDocumentScan ${site}`, async ({ I }) => {
-        const fs = require('fs');
         const requestUrl = `/SmartDealApi/SendDocumentScan`;
-        const filePath = 'D:\\Project\\testFile.pdf';
         const flags: IFlags = getFlags(false, true, true);
         const headers = {};
+        //нужно передавать массивы в base64
+        const byteDocument = [49, 48, 58, 50, 52, 58, 50, 54]
+        const base64String = btoa(String.fromCharCode.apply(null, byteDocument));
         const body = {
-            "DocumentType": 0,
-            "ByteDocument":  fs.createReadStream(filePath)
+            DocumentType: 0,
+             ByteDocument: base64String
         }
          const response = await performRequest(I, site, requestUrl, flags, headers, body);
          console.log(util.inspect(response.data), { depth: null, colors: true });
